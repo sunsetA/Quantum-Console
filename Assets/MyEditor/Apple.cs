55a1f6119c6398783b9a4311fd89fd54572624d8
huafeng <ai29027623@vip.qq.com>
2025-02-14 08:51:46 +0800

    modified and add rope plugins

---
+++ Assets/MyEditor/Apple.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*转为UTF-8*/
+public class Apple : ScriptableObject
+{
+
+}
---
+++ Assets/MyEditor/AppleFactor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*转为UTF-8*/
+public class AppleFactor : MonoBehaviour
+{
+    public ScriptableObject scriptable;
+}
+
+
